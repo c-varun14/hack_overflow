c68c55965a9f96684d317a3287ad947ef79224f9
kunal896 <50267369+kunal896@users.noreply.github.com>
2023-05-27 13:29:45 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar/Navbar";
+import Signup from "../pages/createUser/Signup";
+import Signin from "../pages/signin/singin";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Signin />
+    </>
+  );
+}
+
+export default App;
